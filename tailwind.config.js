54bf2c29f75b02390e917e44f2b8e7fda6da1c9c
Sohil Lochan <sohil09956@gmail.com>
2024-07-01 01:11:56 +0530

    developed and made quite a lot of changes along with menu.html

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+        fontFamily: {
+          playwrite: ['Playwrite PL', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
